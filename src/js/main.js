669adf8d9b5f694bae10125392d3116a2c412178
Jordan <i.aslanoglou@gazzetta.gr>
2024-04-23 12:14:48 +0300

    vite-bootstrap

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+// import './assets/main.css'
+import '../scss/styles.scss'
+
+import { createApp } from 'vue'
+import App from '../App.vue'
+
+import 'primevue/resources/themes/lara-dark-teal/theme.css'
+
+import PrimeVue from 'primevue/config'
+
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+const app = createApp(App)
+
+app.use(PrimeVue, { ripple: true })
+
+app.component('Button', Button)
+app.component('InputText', InputText)
+app.mount('#app')
